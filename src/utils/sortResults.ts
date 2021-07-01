72278533ddb239b9b311b24b3249912a81d9b7ff
Hieu C. Nguyen <nc.hieu@live.com>
2021-07-01 08:49:24 +0700

    Update sort snippet results

---
+++ src/utils/sortResults.ts
@@ -0,0 +1,57 @@
+import { JSDOM } from "jsdom";
+import { FetchPageResult } from "./fetchPageContent";
+
+export type SnippetResult = {
+    votes: number,
+    code: string,
+    hasCheckMark: boolean,
+    // textContent: string 
+}
+
+export type SnippetPageResult = {
+    results: SnippetResult[],
+    url: string
+}
+
+// Extract and sort stackoverflow answers
+export function extractSnippetResults(options: FetchPageResult): SnippetPageResult {
+    var doc = new JSDOM(options.textContent)
+
+    let answersWithCodeBlock = Array.from(doc.window.document.querySelectorAll(".answer"))
+        .filter((item: any) => item.querySelector("code") != null)
+
+    let results = answersWithCodeBlock
+        .map((item: any) => ({
+            textContent: item.textContent,
+            votes: parseInt(item.querySelector(".js-vote-count").textContent),
+
+            // TODO: Handle answers with more than one code block
+            // p/s: they often about explaining the something
+            code: item.querySelector("code").textContent,
+            hasCheckMark: item.querySelector("iconCheckmarkLg") != null
+        }) as SnippetResult)
+        .filter(item => isCodeValid(item.code))
+
+
+    results.sort(sortSnippetResultFn)
+
+    return { url: options.url, results }
+}
+
+function sortSnippetResultFn(a: SnippetResult, b: SnippetResult) {
+
+    if (a.hasCheckMark != b.hasCheckMark) {
+        return a.hasCheckMark ? 1 : -1;
+    }
+
+    let result = b.votes - a.votes;
+    return result === 0 ? b.code.length - a.code.length : result
+}
+
+// Check whether the input should be considered as code input or random text
+function isCodeValid(input: string) {
+
+    // This is just a temporary solution,
+    // it would filter codes that are too short
+    return input.length > 12
+}
