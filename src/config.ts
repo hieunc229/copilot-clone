8740592158ff29d8698fe38881742332dad47fc3
Charlie Lin <clin@rollins.edu>
2021-07-17 17:43:36 -0400

    Add possible search phrase starting with `#`

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const CSConfig = {
     SEARCH_ENDPOINT: `https://www.google.com/search?q=site%3Astackoverflow.com+`,
-    SEARCH_PHRASE_START: `//find`,
+    SEARCH_PHRASE_START: [`//find`, `#find`],
     SEARCH_PHRASE_END: `.`
 }
 
