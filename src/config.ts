e3fafa4d98e521fa597e31d9d1af4907798a6d9e
Hieu Nguyen (Jack) <nc.hieu@live.com>
2021-07-17 21:17:05 +0700

    Update config.ts
    Update `SEARCH_PHRASE_START` variable

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const CSConfig = {
     SEARCH_ENDPOINT: `https://www.google.com/search?q=site%3Astackoverflow.com+`,
-    SEARCH_PHRASE: `//find`,
+    SEARCH_PHRASE_START: `//find`,
     SEARCH_PHRASE_END: `.`
 }
 
