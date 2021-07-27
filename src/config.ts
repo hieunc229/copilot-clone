29cfbd75fd0d98bd2c36edc2ef1fdb60f685cfcf
Hieu C. Nguyen <nc.hieu@live.com>
2021-07-27 23:07:27 +0700

    improve SEARCH_PATTERN to match with more languages

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const CSConfig = {
     SEARCH_ENDPOINT: `https://www.google.com/search?q=site%3Astackoverflow.com+`,
-    SEARCH_PATTERN: /\/\/find\s?(.+)\./
+    SEARCH_PATTERN: /(\/\/|#|--|<!--)\s?find\s?(.+)\./
 };
 
 export default CSConfig;
