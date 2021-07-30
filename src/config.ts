20386cdaa151141614dc21a9ff7eb3a54a8fb3cb
Hieu C. Nguyen <nc.hieu@live.com>
2021-07-30 09:52:39 +0700

    [regex] improve pattern to match the comment closing syntax

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const CSConfig = {
     SEARCH_ENDPOINT: `https://www.google.com/search?q=site%3Astackoverflow.com+`,
-    SEARCH_PATTERN: /(\/\/|#|--|<!--|!)\s?find\s?(.+)\./
+    SEARCH_PATTERN: /(\/\/|#|--|<!--)\s?find\s?(.+)\s?(\.|-->)/
 };
 
 export default CSConfig;
