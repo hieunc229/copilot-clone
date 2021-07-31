da7c6a7c7593a25cef5073c3dca6b2800bda0d95
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2021-07-31 18:49:40 +0200

    Added info on what is happening
    Show a status update in the bottom left corner telling the user when captain stack is loading something or has finished doing so

--- out/utils/search.js
+++ out/utils/search.js
@@ -4,6 +4,7 @@ exports.search = void 0;
 const extractGoogleResults_1 = require("./extractGoogleResults");
 const extractStackOverflowResults_1 = require("./extractStackOverflowResults");
 const fetchPageContent_1 = require("./fetchPageContent");
+const vscode = require("vscode");
 // Send search query to google, get answers from stackoverflow
 // then extract and return code results
 async function search(keyword) {
@@ -18,9 +19,19 @@ async function search(keyword) {
                 let fetchResult;
                 for (const i in urls.splice(0, 6)) {
                     if (urls[i]) {
-                        fetchResult = await fetchPageContent_1.fetchPageTextContent(urls[i]);
+                        // A promise for vscode to stop showing the status bar message when resolved with the FetchPageResult and then show message with attached promise
+                        // so the message will be hidden again when promise has been resolved.
+                        let promise = new Promise((resolve, reject) => {
+                            resolve(fetchPageContent_1.fetchPageTextContent(urls[i]));
+                        });
+                        vscode.window.setStatusBarMessage("Loading Captain Stack results...", promise);
+                        fetchResult = await promise;
+                        // When promise resolved, show finished loading for 5 seconds
+                        vscode.window.setStatusBarMessage("Finished loading results", 5000);
                         results = results.concat(extractStackOverflowResults_1.extractSnippetResults(fetchResult).results);
                     }
+                }
+                for (const y in urls) {
                 }
                 resolve({ results });
             }
--- src/utils/search.ts
+++ src/utils/search.ts
@@ -1,6 +1,7 @@
 import { extractGoogleResults } from "./extractGoogleResults";
 import { extractSnippetResults, SnippetResult } from "./extractStackOverflowResults";
 import { fetchPageTextContent } from "./fetchPageContent";
+import * as vscode from 'vscode';
 
 // Send search query to google, get answers from stackoverflow
 // then extract and return code results
@@ -21,9 +22,20 @@ export async function search(keyword: string): Promise<null | { results: Snippet
 
                     for (const i in urls.splice(0, 6)) {
                         if (urls[i]) {
-                            fetchResult = await fetchPageTextContent(urls[i]);
+                            // A promise for vscode to stop showing the status bar message when resolved with the FetchPageResult and then show message with attached promise
+                            // so the message will be hidden again when promise has been resolved.
+                            let promise = new Promise<{ textContent: string, url: string }>((resolve, reject) => {
+                                resolve(fetchPageTextContent(urls[i]));
+                            });
+                            vscode.window.setStatusBarMessage("Loading Captain Stack results...", promise);
+                            fetchResult = await promise;
+                            // When promise resolved, show finished loading for 5 seconds
+                            vscode.window.setStatusBarMessage("Finished loading results", 5000);
                             results = results.concat(extractSnippetResults(fetchResult).results);
                         }
+                    }
+
+                    for (const y in urls) {
                     }
 
                     resolve({ results });
