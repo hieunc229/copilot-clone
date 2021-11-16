474772e4f3ca54a107532793df6192f9b708429a
Dawn Drain <dawn@anthropic.com>
2021-11-16 15:49:34 -0800

    delete some more unused files

--- out/utils/extractors/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ExtractorGithubGist_1 = require("./ExtractorGithubGist");
-const ExtractorStackOverflow_1 = require("./ExtractorStackOverflow");
-const SnippetExtractors = [
-    new ExtractorStackOverflow_1.default(),
-    new ExtractorGithubGist_1.default()
-];
-exports.default = SnippetExtractors;
