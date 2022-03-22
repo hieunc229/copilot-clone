1ab2db13dc7ba47e6bbd676c58d0bb6aa1848262
Kekschen <52585984+Kek5chen@users.noreply.github.com>
2022-03-22 10:46:18 +0100

    Updated AI. Tested 100 samples from stack overflow yielding 99% precision.

--- src/utils/extractors/utils.ts
+++ src/utils/extractors/utils.ts
@@ -1,7 +1,12 @@
-const keywords = ["def", "void", "int", "if", "else", "while", "return", "true", "false", "null", "this"];
+const keywords = ["def", "void", "int", "if", "else", "while", "return", "true", "false", "null", "this.", "print", "do", "", ""];
+const high_confidence_keywords = ['#!/bin/bash', 'select *', 'select ?', '* from', '? from', 'where', 'order by', 'group by', 'left join', 'right join', 'inner join', 'outer join', 'exec '];
+const symbol_keywords = ["=>", "==", "!=", ">", "<", ">=", "<=", "&&", "||"];
+const low_confidence_keywords = ['trace', 'error', 'exception', 'warn'];
 
 // Check whether the input should be considered as code input or random text
 export function isCodeValid(input: string): boolean {
+    input = input.toLowerCase();
+
     const openAngle = input.split('<').length - 1;
     const closedAngle = input.split('>').length - 1;
 
@@ -14,8 +19,8 @@ export function isCodeValid(input: string): boolean {
     const openParen = input.split('(').length - 1;
     const closedParen = input.split(')').length - 1;
 
-    const total = openAngle + closedAngle + openBrackets + closedBrackets + openCurly + closedCurly + openParen + closedParen;
-
+    const bracketTotal = openAngle + closedAngle + openBrackets + closedBrackets + openCurly + closedCurly + openParen + closedParen;
+    // calculate the inequality of brackets. the lower the number the more unequal
     const inequality = -Math.abs(openAngle - closedAngle) -
         Math.abs(openBrackets - closedBrackets) -
         Math.abs(openCurly - closedCurly) -
@@ -24,22 +29,49 @@ export function isCodeValid(input: string): boolean {
     const semicolons = input.split(';').length - 1;
     const colons = input.split(':').length - 1;
 
-    let keywordsFound = 0;
+    const quotes = input.split('"').length - 1;
+    const singleQuotes = input.split("'").length - 1;
 
-    input.split(' ').forEach(word => {
+    let keywordsFound = 0;
+    let highKeywordsFound = 0;
+    let lowKeywordsFound = 0;
+    let symbolsFound = 0;
+
+    // test keywords
+    input.split(' ').map(n => n.replace('\n', '')).filter(n => n != '').forEach(word => {
         if (keywords.includes(word)) {
             keywordsFound++;
-            return false;
+        }
+    });
+    high_confidence_keywords.forEach(word => {
+        if (input.includes(word)) {
+            highKeywordsFound++;
+        }
+    });
+    low_confidence_keywords.forEach(word => {
+        if (input.includes(word)) {
+            lowKeywordsFound++;
+        }
+    });
+    symbol_keywords.forEach(symbol => {
+        if (input.includes(symbol)) {
+            symbolsFound++;
         }
     });
 
     let confidence = 0;
-    confidence += total > 0 ? .5 : -.5;
-    confidence += input.length > 6 ? .5 : -.1;
-    confidence += inequality < 0 ? inequality / 10 : total == 0 ? 0 : .6 * total / 2;
-    confidence += semicolons > 0 ? .5 : -.5;
+    confidence += bracketTotal > 0 ? .5 : -.5;
+    confidence += input.length > 8 ? .5 : -.3;
+    confidence += (inequality < 0 ? inequality / 10 : bracketTotal == 0 ? 0 : .3 * bracketTotal) * (20 / input.length);
+    confidence += semicolons > 0 ? 1 : -.5;
     confidence += colons > 0 ? .2 : -.2;
-    confidence += keywordsFound > 0 ? .7 : 0;
+    confidence += keywordsFound * .7;
+    confidence += 3 * highKeywordsFound;
+    confidence += -2 * lowKeywordsFound;
+    confidence += symbolsFound;
+    confidence += input.endsWith(';') ? .7 : 0;
+    confidence += quotes > 0 ? .5 : 0;
+    confidence += singleQuotes > 0 ? .5 : 0;
 
     return confidence >= 1;
 }
