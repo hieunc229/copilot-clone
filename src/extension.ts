8da1fd954117d5d8a371554f7d4d2e5acc0a16f3
Hieu Nguyen <nc.hieu@live.com>
2022-07-02 12:24:12 +0700

    Minor code improvement

--- src/extension.ts
+++ src/extension.ts
@@ -5,44 +5,39 @@ import { matchSearchPhrase } from './utils/matchSearchPhrase';
 
 export function activate(_: vscode.ExtensionContext) {
 
-	const provider: vscode.InlineCompletionItemProvider<vscode.InlineCompletionItem> = {
+	const provider: vscode.CompletionItemProvider = {
+    // @ts-ignore
 		provideInlineCompletionItems: async (document, position, context, token) => {
+
 			const textBeforeCursor = document.getText(
 				new vscode.Range(position.with(undefined, 0), position)
 			);
 
 			const match = matchSearchPhrase(textBeforeCursor);
+      let items: any[] = [];
 
 			if (match) {
-
 				let rs;
-
 				try {
 					rs = await search(match.searchPhrase);
-				} catch (err) {
+          if (rs) {
+            items = rs.results.map(item => {
+              const output = `\n${match.commentSyntax} Source: ${item.sourceURL} ${match.commentSyntaxEnd}\n${item.code}`;
+              return {
+                text: output,
+                insertText: output,
+                range: new vscode.Range(position.translate(0, output.length), position)
+              };
+            });
+          }
+				} catch (err: any) {
 					vscode.window.showErrorMessage(err.toString());
-					return { items: [] };
 				}
-
-				if (rs == null) {
-					return { items: [] };
-				}
-
-				const items = rs.results.map(item => {
-					const output = `\n${match.commentSyntax} Source: ${item.sourceURL} ${match.commentSyntaxEnd}\n${item.code}`;
-					return {
-						text: output,
-            insertText: output,
-						range: new vscode.Range(position.translate(0, output.length), position)
-					} as vscode.InlineCompletionItem;
-				});
-
-				return { items };
 			}
-
-			return { items: [] };
+			return { items };
 		},
 	};
 
+  // @ts-ignore
 	vscode.languages.registerInlineCompletionItemProvider({ pattern: "**" }, provider);
 }
