1663ad9fa60cf0b75119b8753a4cd549bce377c5
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-08-22 22:09:42 +0430

    Implemented parsers

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,12 @@
 import * as vscode from 'vscode';
-import * as lang from './languages';
 
-try { process.removeAllListeners('warning'); } catch (e) { /* browser, probably. */ }
+import { search } from './utils/search';
+import { matchSearchPhrase } from './utils/matchSearchPhrase';
+import { getConfig } from './config';
+import Parsers from './utils/parsers/index';
 
-const DEBUG = false;
-
-let run : Boolean = false
-
-/**
- * Sends an HTTP request to the huggingface bloom API and returns the processed response.
- * @param {String} text The text for the AI to compute.
- * @param {Number} seed 
- * @param {Boolean} early_stopping 
- * @param {Number} length_penalty 
- * @param {Number} max_new_tokens 
- * @param {Boolean} do_sample 
- * @param {Number} top_p 
- * @returns {String} The text returned by the AI.
- */
-const bloom = async (text : string, seed = 31, early_stopping = false, length_penalty = 0, max_new_tokens = 20, do_sample = false, top_p = 0.9) => {
-    const response = await fetch("https://api-inference.huggingface.co/models/bigscience/bloom", {
-        "headers": {
-            'Authorization': 'Bearer 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'
-        },
-        "body": JSON.stringify({ 
-            "inputs": text,
-            "parameters": {
-                "seed": seed,
-                "early_stopping": early_stopping,
-                "length_penalty": length_penalty,
-                "max_new_tokens": max_new_tokens,
-                "do_sample": do_sample,
-                "top_p": top_p
-            }
-        }),
-        "method": "POST"
-    });
-    const res : Array<any> = await response.json();
-    DEBUG && console.log('Bloom: ', res)
-
-    return res.map((e : any) => e.generated_text ? e.generated_text : e[0].generated_text).join(' ');
-};
+let runningParsers = false;
+let cancelEvent = false;
 
 export function activate(_: vscode.ExtensionContext) {
 
@@ -48,31 +14,79 @@ export function activate(_: vscode.ExtensionContext) {
         // @ts-ignore
         provideInlineCompletionItems: async (document : vscode.TextDocument, position, context, token) => {
 
-            const textBeforeCursor = 'Language: ' + lang.languages[document.fileName.split('.').pop() || ''] + '\n\n' + document.getText(
-                new vscode.Range(position.with(0, 0), position)
+            const textBeforeCursor = document.getText(
+                new vscode.Range(position.with(undefined, 0), position)
             );
 
-            if(run) {
-                await new Promise((resolve, reject) => {
-                    setInterval(() => {
-                        if(!run) resolve(null);
-                    }, 10)
-                })
+            const match = matchSearchPhrase(textBeforeCursor);
+            let items: any[] = [];
 
-                run = false;
-            }
-
-            DEBUG && console.log('Bloom: Fetching data...')
-            const output = (await bloom(textBeforeCursor)).substring(textBeforeCursor.length);
-            DEBUG && console.log('Bloom: Done!')
-
-            const items = [
-                {
-                    text: output,
-                    insertText: output,
-                    range: new vscode.Range(position.translate(0, output.length), position)
+            if (match) {
+                let rs;
+                try {
+                    rs = await search(match.searchPhrase);
+                    if (rs) {
+                        items = rs.results.map(item => {
+                            const output = `\n${match.commentSyntax} Source: ${item.sourceURL} ${match.commentSyntaxEnd}\n${item.code}`;
+                            return {
+                                text: output,
+                                insertText: output,
+                                range: new vscode.Range(position.translate(0, output.length), position)
+                            };
+                        });
+                    }
+                } catch (err: any) {
+                    vscode.window.showErrorMessage(err.toString());
                 }
-            ];
+            } else {
+                /* check if parsers are enabled */
+                if(getConfig().settings.enableParsers) {
+                    if(runningParsers) cancelEvent = true;
+
+                    runningParsers = true;
+                    /* result has to be determined using parsers. */
+                    const parserText = document.getText(
+                        new vscode.Range(position.with(0, 0), position)
+                    );
+
+                    /* find enabled parsers and ask them for a result! */
+                    Parsers.filter((parser) => parser.isEnabled())
+                    .forEach(async (parser) => {
+
+                        parser.provideInlineCodeCompletions(
+                            parserText,
+                            document.fileName,
+                            getConfig().settings.huggingfaceToken
+                        ).then((result) => {
+                            items.push({
+                                text: result,
+                                insertText: result,
+                                range: new vscode.Range(position.translate(0, result.length), position)
+                            });
+                        })
+                        
+                        return await new Promise((resolve, reject) => {
+                            setInterval(() => {
+                                if(cancelEvent) resolve();
+                            }, 100);
+                        });
+                    });
+
+                    cancelEvent = false;
+                    await new Promise((resolve, reject) => {
+                        let iterations = 0;
+                        setInterval(() => {
+                            if(cancelEvent) reject(null);
+
+                            iterations++;
+                            if(items.length === Parsers.length) resolve(null);
+                            else if (iterations > (5 * 10)) resolve(null);
+                        }, 100);
+                    });
+
+                    runningParsers = false;
+                }
+            }
 
             return {items};
         },
