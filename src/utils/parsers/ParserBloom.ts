42c8388d9834d1c9b7fe633c1cd590e1321b0db2
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2022-08-23 13:58:39 +0430

    Updated bloom parser to include a ratelimit message and a lock.

--- src/utils/parsers/ParserBloom.ts
+++ src/utils/parsers/ParserBloom.ts
@@ -1,6 +1,10 @@
 import ParserAbstract from './ParserAbstract';
 import * as lang from './languages';
 import fetch from 'node-fetch';
+import * as vscode from 'vscode';
+
+let sentThisSession = false;
+let apiLock = false;
 
 /**
  * Sends an HTTP request to the huggingface bloom API and returns the processed response.
@@ -13,37 +17,53 @@ import fetch from 'node-fetch';
  * @param {Number} top_p 
  * @returns {String} The text returned by the AI.
  */
- const bloom = async (text : string, token  = '', seed  = 31, early_stopping  = false, length_penalty  = 0, max_new_tokens = 20, do_sample = false, top_p  = 0.9) => {
-    const response = await fetch("https://api-inference.huggingface.co/models/bigscience/bloom", {
-        "headers": {
-            'Authorization': 'Bearer ' + token
-        },
-        "body": JSON.stringify({ 
-            "inputs": text,
-            "parameters": {
-                "seed": seed,
-                "early_stopping": early_stopping,
-                "length_penalty": length_penalty,
-                "max_new_tokens": max_new_tokens,
-                "do_sample": do_sample,
-                "top_p": top_p
-            }
-        }),
-        "method": "POST"
-    });
-    const res : Array<any> = await response.json();
+const bloom = async (text: string, token = '', seed = 31, early_stopping = false, length_penalty = 0, max_new_tokens = 20, do_sample = false, top_p = 0.9) => {
+    if (!apiLock && Math.random() > 0.5) {
+        apiLock = true;
+        const response = await fetch("https://api-inference.huggingface.co/models/bigscience/bloom", {
+            "headers": {
+                'Authorization': token !== '' ? 'Bearer ' + token : ''
+            },
+            "body": JSON.stringify({
+                "inputs": text,
+                "parameters": {
+                    "seed": seed,
+                    "early_stopping": early_stopping,
+                    "length_penalty": length_penalty,
+                    "max_new_tokens": max_new_tokens,
+                    "do_sample": do_sample,
+                    "top_p": top_p
+                }
+            }),
+            "method": "POST"
+        });
+        const res: any = await response.json();
 
-    return res.map((e : any) => e.generated_text ? e.generated_text : e[0].generated_text).join(' ');
+        if (res.map) {
+            apiLock = false;
+            return res.map((e: any) => e.generated_text ? e.generated_text : e[0].generated_text).join(' ');
+        } else {
+            if (!sentThisSession) {
+                sentThisSession = true;
+                vscode.window.showErrorMessage('Reached free ratelimit for bloom usage...');
+            }
+
+            apiLock = false;
+            return '';
+        }
+    } else {
+        return '';
+    }
 };
 
 export default class ParserBloom extends ParserAbstract {
     name = 'Bloom'
 
-    provideInlineCodeCompletions = (text : string, filename : string, huggingface_token  = '') => {
+    provideInlineCodeCompletions = (text: string, filename: string, huggingface_token = '') => {
         return new Promise<string>(async (resolve, reject) => {
-            const textBeforeCursor = 'Language: ' + lang.languages[filename.split('.').pop() || ''] + '\n\n' + text;
+            const Language = lang.languages[filename.split('.').pop() || ''];
+            const textBeforeCursor = (Language !== '' ? 'Language: ' + Language + '\n\n' : '') + text;
             let output = (await bloom(textBeforeCursor, huggingface_token)).substring(textBeforeCursor.length);
-            while(output.startsWith('\n')) output = output.substring(1);
 
             resolve(output);
         });
