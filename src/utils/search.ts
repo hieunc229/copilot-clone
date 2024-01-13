aac53f8caa214dd1e1d84dcdb7141adb065a140e
Matthew Wallace <wallacm4@tcd.ie>
2024-01-13 03:31:12 +0000

    Input Handled

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -3,6 +3,10 @@ import * as vscode from 'vscode';
 import bmi from './resources/BMI.txt';
 import deviation from './resources/AverageStandardDeviation.txt';
 import minmax from './resources/MinMax.txt';
+import multiples from './resources/Multiples.txt';
+import coin from './resources/TossCoin.txt';
+import area from './resources/TriangleArea.txt';
+import vert from './resources/Vertebrates.txt';
 
 /**
  * Cache results to avoid VSCode keep refetching
@@ -22,22 +26,19 @@ export async function search(keyword: string): Promise<null | { results: string[
         console.log('Searching:', keyword);
         console.log('File Contents', minmax);
         const results: string[] = [];
+        const solutions: { [key: string]: string } = { bmi, deviation, minmax, multiples, coin, area, vert };
 
         try {
-            // const files = fs.readdirSync('resources');
-            // files.forEach((file) => {
-            //     console.log("File Name: ", file);
-            //     if(file.toLowerCase().includes(keyword))
-            //         results.push(fs.readFileSync(file, 'utf-8'));
-        
-            // });
-            results.push(minmax);
+            Object.keys(solutions).forEach(key => {
+                if((keyword).includes(key))
+                    results.push(solutions[key]);
+            });
+            
             resolve({ results });
         } catch (error) {
             console.error('Error reading directory:', error);
             reject(error);
         }
-        
 
         // When promise resolved, show finished loading for 5 seconds
         vscode.window.setStatusBarMessage(`CommandPilot: Finished loading ${results.length} results`);
